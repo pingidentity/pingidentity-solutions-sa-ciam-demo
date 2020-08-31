6fa035ccbf3d21ca6e58921fee2fa0c1cc4bdc65
Chris Price <cprice@pingidentity.com>
2020-08-31 14:55:14 -0700

    Updating PingID IK (for Internal flows)

--- pingfederate/hooks/50-before-post-start.sh
+++ pingfederate/hooks/50-before-post-start.sh
@@ -25,11 +25,15 @@ sed -e "s#<construct class=\"org.sourceid.oauth20.domain.ClientManagerXmlFileImp
 mv /opt/out/instance/server/default/conf/META-INF/hivemodule.xml-modified /opt/out/instance/server/default/conf/META-INF/hivemodule.xml
 
 # Delete bundled files so that Server Profile can apply newer ones
-echo #### Removing bundled file - /opt/out/instance/server/default/lib/pf-authn-api-sdk-1.0.0.48.jar
+echo Removing bundled files
+# AuthN API
+echo PF AuthN API
 rm /opt/out/instance/server/default/lib/pf-authn-api-sdk-1.0.0.48.jar
-
-ls /opt/out/instance/server/default/lib
-
+echo PingID IK
+# PingID IK
+rm /opt/out/instance/server/default/deploy/pf-pingid-idp-adapter-2.6.jar
+rm /opt/out/instance/server/default/deploy/pf-pingid-quickconnection-1.0.1.jar
+rm /opt/out/instance/server/default/deploy/PingIDRadiusPCV-2.5.0.jar
 # echo "##########
 # "
 # cat /opt/out/instance/server/default/conf/META-INF/hivemodule.xml
